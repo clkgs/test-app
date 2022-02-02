e467179cbad5fd3c8426c1e447b3bd4323ecfba3
Pavel Molotkov <cl@kgs.ru>
2022-02-02 12:06:10 +0700

    Update default route and view for SPA

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <title></title>
+</head>
+<body>
+<div id="app">
+    <app></app>
+</div>
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
